3c8ea38ba75d4bfb24a25ea4107e4f334e1de9c0
Sebastian <Sebastian@lindback.pro>
2021-12-10 00:12:07 +0100

    Data storage

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 (function() {
-   setMinDate();
+
 
 })();
 
@@ -11,6 +11,31 @@ function setMinDate(){
   document.getElementsByClassName('setMinDate')[0].min = year + "-" + month;
   console.log(`min date set for ${document.getElementsByClassName('setMinDate')[0].name}  (${year} - ${month})`);
 }
-function submitContact(){
-  console.log(document.getElementsByClassName('contactForm')[0], 22);
+function displayContact() {
+  console.log("Load");
+  let data = localStorage.getItem("customerinfo");
+  console.log(document.getElementsByClassName('contactDisplay')[0]
+  );
+  if(document.getElementsByClassName('contactDisplay')[0]){ document.getElementsByClassName('contactDisplay')[0].innerText = data;}
+  console.log(data);
+  console.log(localStorage.getItem("customerPayment"));
+}
+
+function getCustomerInfo(formtype) {
+  let info = "{";
+  let form = document.getElementsByClassName(formtype)[0];
+
+  for (let i = 0; i < form.getElementsByTagName('input').length; i++) {
+    console.log(form.getElementsByTagName('input')[i].value);
+    if (i == form.getElementsByTagName('input').length -1 ){
+      info += "'" + form.getElementsByTagName('input')[i].name + "':'" + form.getElementsByTagName('input')[i].value + "'}";
+    }
+    else {
+      info += "'" + form.getElementsByTagName('input')[i].name + "':'" + form.getElementsByTagName('input')[i].value + "',";
+    }
+
+  }
+  if (formtype == 'contactForm'){localStorage.setItem("customerinfo", info);}
+  else {localStorage.setItem("customerPayment", info); }
+  console.log(localStorage.getItem("payment", "customerPayment"), localStorage.getItem("info", "customerinfo"));
 }
